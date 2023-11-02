53f69143246b572a0518fa1861c3983787ab0a3b
RFozzy688 <pokerfozzy@gmail.com>
2023-11-02 18:38:46 +0200

    поток SumNumbersThread

--- ManualResetEvent_task_1/Program.cs
+++ ManualResetEvent_task_1/Program.cs
@@ -10,27 +10,29 @@ namespace ManualResetEvent_task_1
 {
     internal class Program
     {
-        string _pathNumbers = "numbers.txt";
-        string _pathSumNumbers = "sumNumbers.txt";
         string _pathMultiplyNumbers = "multiplyNumbers.txt";
         int[,] _arrNumbers = new int[5, 2];
         Thread _generateNumbersThread;
         Thread _sumNumbersThread;
         Thread _multiplyNumbersThread;
-        ManualResetEvent _manualReset = new ManualResetEvent(true);
+        AutoResetEvent _autoReset = new AutoResetEvent(true);
         static void Main(string[] args)
         {
             Program pr = new Program();
 
-            ThreadStart threadStart1 = new ThreadStart(pr.GenerateSaveNumbersThread);
-            pr._generateNumbersThread = new Thread(threadStart1);
-            pr._generateNumbersThread.Start();
+            ThreadStart threadStart = new ThreadStart(pr.GenerateSaveNumbersThread);
+            Thread generateNumbersThread = new Thread(threadStart);
+            generateNumbersThread.Start();
+
+            ParameterizedThreadStart parameterThread1 = new ParameterizedThreadStart(pr.SumNumbersThread);
+            Thread sumNumbersThread = new Thread(parameterThread1);
+            sumNumbersThread.Start("sumNumbers.txt");
 
             Console.ReadLine();
         }
         void GenerateSaveNumbersThread()
         {
-            _manualReset.WaitOne();
+            _autoReset.WaitOne();
 
             Random rnd = new Random();
 
@@ -42,18 +44,74 @@ namespace ManualResetEvent_task_1
                 }
             }
 
-            using (FileStream fs = new FileStream(_pathNumbers, FileMode.Create))
+            using (FileStream fs = new FileStream("numbers.txt", FileMode.Create))
             {
                 using (StreamWriter sw = new StreamWriter(fs))
                 {
                     for (int i = 0; i < _arrNumbers.GetLength(0); i++)
                     {
-                        sw.WriteLine(_arrNumbers[i, 0].ToString() + " " + _arrNumbers[i, 1].ToString());
+                        sw.WriteLine(_arrNumbers[i, 0] + " " + _arrNumbers[i, 1]);
                     }
                 }
             }
 
-            _manualReset.Reset();
+            _autoReset.Reset();
+            _autoReset.Set();
+        }
+        void SumNumbersThread(object obj)
+        {
+            _autoReset.WaitOne();
+
+            string path = obj as string;
+
+            Array.Clear(_arrNumbers, 0, 10);
+            LoadNumbers();
+
+            int[] arrSum = new int[5];
+
+            for (int i = 0; i < _arrNumbers.GetLength(0); i++)
+            {
+                arrSum[i] = _arrNumbers[i, 0] + _arrNumbers[i, 1];
+            }
+
+            SaveNumbers(path, arrSum);
+
+            _autoReset.Reset();
+            _autoReset.Set();
+        }
+        void LoadNumbers()
+        {
+            int row = 0;
+
+            using (FileStream fs = new FileStream("numbers.txt", FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string str = sr.ReadLine();
+                        string[] numbers = str.Split(' ');
+
+                        _arrNumbers[row, 0] = Int32.Parse(numbers[0]);
+                        _arrNumbers[row, 1] = Int32.Parse(numbers[1]);
+
+                        row++;
+                    }
+                }
+            }
+        }
+        void SaveNumbers(string path, int[] arr)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        sw.WriteLine(arr[i]);
+                    }
+                }
+            }
         }
     }
 }
